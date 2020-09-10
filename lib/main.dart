d5b5459f7eb3ac48a918a05a8fa5ee4a51c06fb5
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-09-09 23:32:48 -0400

    Named Routes Made

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/welcome_screen.dart';
@@ -5,7 +8,6 @@ import 'screens/welcome_screen.dart';
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +16,14 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.ID,
+      routes: {
+        WelcomeScreen.ID: (context) => WelcomeScreen(),
+        LoginScreen.ID: (context) => LoginScreen(),
+        ChatScreen.ID: (context) => ChatScreen(),
+        RegistrationScreen.ID: (context) => RegistrationScreen()
+      },
+      //home: WelcomeScreen(),
     );
   }
 }
