5343e05ac0644c5fc881e03336587160e42e755f
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-09-09 23:33:04 -0400

    Hero Animation Implemented

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
+  static String ID = 'loginScreen';
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
